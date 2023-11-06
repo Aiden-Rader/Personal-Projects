d1614e587891d5e2e3a40d421a8a8156a00fec7e
Aiden Rader <101580778+Aiden-Rader@users.noreply.github.com>
2023-11-05 22:14:53 -0500

    Added Face Detection Project Using OpenCV

---
+++ OpenCV Training.cpp
@@ -0,0 +1,15 @@
+// OpenCV Training.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "FaceDetection.h"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
